2f7355d133db738a44fe645357078038dac96f83
Scott <scottharben@gmail.com>
2020-11-17 13:46:00 -0500

    text push

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
   render() {
     return (
       <React.Fragment>
+        {/* Hello! */}
         <Navbar />
         <div id="Parent">
           <div className="container-fluid Content">
