e7b10726a5ac7a7bc2c93a24d1f2e9a24d27fdbf
Scott <scottharben@gmail.com>
2020-11-17 13:50:12 -0500

    mobileFirst hello2

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
   render() {
     return (
       <React.Fragment>
+        {/* hello 2 */}
         <Navbar />
         <div id="Parent">
           <div className="container-fluid Content">
